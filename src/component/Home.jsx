e125ab0e72dc8358e080913138e0643e07e9127b
Kar9ti8k <karthiksankineni07@gmail.com>
2024-10-21 23:21:06 +0530

    home l remove

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,7 +3,7 @@ import { AiFillHome } from 'react-icons/ai'
 const Home = () => {
   return (
     <div className='hover:text-amber-500'>
-      <p className='uppercase hidden sm:inline'>Homel</p>
+      <p className='uppercase hidden sm:inline'>Home</p>
       <AiFillHome className='sm:hidden text-3xl' />
     </div>
   )
